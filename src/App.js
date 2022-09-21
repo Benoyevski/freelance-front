f5238381c31c58526a483750035c34c1fd80b609
Surkho <asa8964@mail.ru>
2022-09-21 11:13:12 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import Header from './components/Header/Header';
 import Headerlk from './components/Headerlk';
 import MainPage from './pages/Mainpage/MainPage';
 
 function App() {
   return (
     <>
-    <Headerlk/>
+    <Header />
       <div className='container'>
+     
         <MainPage/>
       </div>
       </>
