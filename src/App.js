9cf9caaa39a4d12e97d059fcdf41089699e8efbf
Waldgang <waldgang@MacBook-Air-salambek.local>
2022-09-21 12:03:30 +0300

    Добавил новую страницу и подключил к ней новый хедер

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-import MainPage from "./pages/Mainpage/MainPage";
 import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/Mainpage/MainPage";
+import TasksPage from "./pages/Taskspage/TasksPage";
+
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage/>}/>
+        <Route path='/tasks' element={<TasksPage/>}/>
       </Routes>
     </>
   );
