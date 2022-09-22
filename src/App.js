19984c15eccea799434cb867d7a3b6fe8d71bba7
Ayub <aub9@mail.ru>
2022-09-22 10:41:34 +0300

    no create

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MainPage from "./pages/Mainpage/MainPage";
 import TasksPage from "./pages/Taskspage/TasksPage";
 import Footer from "./components/Footer/Footer";
 
-
 function App() {
   return (
     <>
