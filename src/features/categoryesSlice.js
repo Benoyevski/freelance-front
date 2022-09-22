f5821b54ea1d357a4e4c6b0f15857350ad1c0ae0
Surkho <asa8964@mail.ru>
2022-09-22 12:01:39 +0300

    Изменил сайдбар

--- src/features/categoryesSlice.js
+++ src/features/categoryesSlice.js
@@ -1,6 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 
 const initialState = {
     category : [],
 }
 
+
+export const fetchCategory = createAsyncThunk("fetch/category",  async (_, thunkAPI) => {
+    try {
+        const res = await fetch("http://localhost:3030/categories");
+        
+        const data  = await res.json()
+
+        return data
+    } catch(e) {
+        return thunkAPI.rejectWithValue(e)
+    }
+})
+
+
+
+
+ const categotySlice =  createSlice({
+    name: "category",
+    initialState,
+    reducers:{},
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchCategory.fulfilled, (state, action) => {
+            state.category = action.payload
+        })
+    }
+ })
+
+
+ export default categotySlice.reducer
