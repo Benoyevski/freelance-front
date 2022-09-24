639f5eb75d29aec4cd47cdb786fe41d2ebc48960
”Ismail” <ibeno95@mail.ru>
2022-09-24 15:45:36 +0300

    сделал отмену отклика на задания

--- src/features/orderSlice.js
+++ src/features/orderSlice.js
@@ -1,81 +1,107 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    orders:[],
-    role: localStorage.getItem("role")
-}
+  orders: [],
+  role: localStorage.getItem("role"),
+};
 
-
-export const follow = createAsyncThunk('patch/order', 
-async ({orderId, id}, thunkAPI) => {
-  try {
-    const res = await fetch(`http://localhost:3030/followOrder/${orderId}`, {
-      method: "PATCH", 
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ user: id} )
-    });
-  
-    return await res.json();
-
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error)
-  }
-})
-
-export const fetchOrders= createAsyncThunk(
-    "fetch/orders",
-    async (_, thunkAPI)=>{
-        try {
-            const res = await fetch("http://localhost:3030/order")
-            const data = await res.json()
-            return data
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error)
-        }
+export const follow = createAsyncThunk(
+  "patch/order",
+  async ({ orderId, id }, thunkAPI) => {
+    try {
+      const res = await fetch(`http://localhost:3030/followOrder/${orderId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user: id }),
+      });
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-)
+  }
+);
 
+export const unFollow = createAsyncThunk(
+  "patch/orderFollow",
+  async ({ orderId, id }, thunkAPI) => {
+    try {
+      const res = await fetch(`http://localhost:3030/unFollow/${orderId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user: id }),
+      });
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const fetchOrders = createAsyncThunk(
+  "fetch/orders",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:3030/order");
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 export const addOrder = createAsyncThunk(
-    "add/orders",
-    async ({creator,price,inner,term,title,location,categoryId }, thunkAPI) => {
-      try {
-        const res = await fetch("http://localhost:3030/order", {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${thunkAPI.getState().application.token}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({creator,price,text:inner,workTime:term,title,location,categoryId }),
-        });
-        
-        const data = await res.json();
-  
-        return data;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
+  "add/orders",
+  async (
+    { creator, price, inner, term, title, location, categoryId },
+    thunkAPI
+  ) => {
+    try {
+      const res = await fetch("http://localhost:3030/order", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${thunkAPI.getState().application.token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          creator,
+          price,
+          text: inner,
+          workTime: term,
+          title,
+          location,
+          categoryId,
+        }),
+      });
+
+      const data = await res.json();
+
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-  );
+  }
+);
 
-
-  const OrderSclice = createSlice({
-    name: "movie",
-    initialState,
-    reducers: {
-
-    },
-    extraReducers: (builder) => {
-      builder
-        .addCase(fetchOrders.fulfilled,(state,action)=>{
-            state.orders = action.payload
-        })
-        .addCase(addOrder.fulfilled,(state,action)=>{
-            state.orders.push(action.payload)
-        })
+const OrderSclice = createSlice({
+  name: "movie",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchOrders.fulfilled, (state, action) => {
+        state.orders = action.payload;
+      })
+      .addCase(addOrder.fulfilled, (state, action) => {
+        state.orders.push(action.payload);
+      })
       //  .addCase(follow.fulfilled, (state, action) => {
       //    state.orders.map((item) => {
       //     if (item._id === action.payload._id) {
@@ -87,14 +113,22 @@ export const addOrder = createAsyncThunk(
       .addCase(follow.fulfilled, (state, action) => {
         state.orders = state.orders.map((item) => {
           if (item._id === action.payload._id) {
-           item.freelancers.push(action.payload)
+            item.freelancers.push(action.payload);
           }
-          return item
+          return item;
         });
-       })
+      })
+      .addCase(unFollow.fulfilled, (state, action) => {
+        state.orders = state.orders.map((item) => {
+          if (item._id === action.payload._id) {
+            item.freelancers.filter((user) => {
+              return user !== action.payload._id
+            });
+          }
+          return item;
+        });
+      })
+  },
+});
 
-    },
-  });
- 
-  
-  export default OrderSclice.reducer;
+export default OrderSclice.reducer;
