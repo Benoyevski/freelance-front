4ee9e5444646ab19a3a1020aad63dd8929068356
Waldgang <waldgang@MacBook-Air-salambek.local>
2022-09-24 17:26:45 +0300

    two

--- src/pages/Orderpage/OrdersPage.jsx
+++ src/pages/Orderpage/OrdersPage.jsx
@@ -4,7 +4,6 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import Order from "../../components/Order/Order";
-import Task from "../../components/Task/Task";
 import SideNavBar from "../../components/SideNavBar/SideNavBar";
 import { fetchOrders } from "../../features/orderSlice";
 import styles from "../Orderpage/orders.module.css";
@@ -19,6 +18,7 @@ const OrderPage = () => {
   return (
     <>
       <Headerlk />
+      
       <div className="container">
         <div className={styles.orders_block}>
          <div> <div className={styles.order_title}>
